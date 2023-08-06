78afac2987da28ece4573af815fc50892fa01340
= <echavemann@gmail.com>
2023-08-05 20:59:23 -0700

    removing cruft

--- caller.cpp
+++ caller.cpp
@@ -1,6 +1,34 @@
 #include <curl/curl.h>
 #include <iostream>
 
+size_t Callback(void* contents, size_t size, size_t nmeb, std::string* output) {
+    size_t total_size = size*nmeb;
+    output->append(static_cast<char*> (contents), total_size);
+    return total_size;
+}
+
+std::string get(const std::string& url) {
+    CURL* curl = curl_easy_init();
+    std::string response;
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, Callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+        CURLcode res = curl_easy_perform(curl);
+        if (res != CURLE_OK) {
+            std::cerr << "Failed to perform request: " << curl_easy_strerror(res) << std::endl;
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+    return response;
+}
+
 int main() {
-    std::cout << "hello father" << std::endl;
+    std::string url = "https://google.com";
+    std::string res = get(url);
+    std::cout << res << std::endl;
 }
